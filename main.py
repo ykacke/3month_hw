869327a2525a99aa5077f0c2e43cec60f052a392
ykacke <153007369+ykacke@users.noreply.github.com>
2024-12-10 15:53:29 +0600

    name random

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from dotenv import dotenv_values
 
-# Получение токена из файла .env
 token = dotenv_values('.env')['BOT-TOKEN']
 bot = Bot(token=token)
 dp = Dispatcher()
 router = Router()
 dp.include_router(router)
 
-# Список имён
-NAME = ("Максим", "Мэри", "Джеймс", "Лора", "Анжела")
+NAME = ("Адина", "Тилек", "Лира", "Кира", "Фатима")
 
-# Команда /random_name
 @router.message(Command("random_name"))
 async def random_name(message: Message):
     random_choice = random.choice(NAME)
     await message.answer(f"Случайное имя: {random_choice}")
 
-# Команда /myinfo
 @router.message(Command("myinfo"))
 async def myinfo(message: Message):
     user = message.from_user
@@ -32,13 +28,11 @@ async def myinfo(message: Message):
     )
     await message.answer(user_info)
 
-# Команда /start
 @router.message(Command("start"))
 async def start_handler(message: Message):
     name = message.from_user.first_name
     await message.answer(f"Привет! {name} приветствует assistant_bot!")
 
-# Основная функция запуска
 async def main():
     # Запуск бота
     await dp.start_polling(bot)
